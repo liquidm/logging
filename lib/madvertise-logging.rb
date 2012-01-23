458eb3061838115e63ce501421732cc437bb8a2c
Benedikt Böhm <bb@xnull.de>
2012-01-23 14:19:39 +0100

    add global logger attribute

--- lib/madvertise-logging.rb
+++ lib/madvertise-logging.rb
@@ -1,3 +1,17 @@
 require 'madvertise/logging/version'
 require 'madvertise/logging/improved_logger'
 require 'madvertise/logging/multi_logger'
+
+module Madvertise
+  class << self
+    # get global logger instance
+    def logger
+      @logger
+    end
+
+    # set global logger instance
+    def logger=(logger)
+      @logger = logger
+    end
+  end
+end
--- lib/madvertise/logging/version.rb
+++ lib/madvertise/logging/version.rb
@@ -1,6 +1,6 @@
 module Madvertise
   module Logging
     # @private
-    VERSION = "0.2.0"
+    VERSION = "0.2.1"
   end
 end
