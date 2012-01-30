62727d5bb9ff6eeaeb8c001dee085e1d77116436
Benedikt Böhm <bb@xnull.de>
2012-01-30 14:58:12 +0100

    Revert "add global logger attribute"
    This reverts commit 458eb3061838115e63ce501421732cc437bb8a2c.
    
    Conflicts:
    
    	lib/madvertise-logging.rb
    	lib/madvertise/logging/version.rb

--- lib/madvertise-logging.rb
+++ lib/madvertise-logging.rb
@@ -1,18 +1,3 @@
 require 'madvertise/logging/version'
 require 'madvertise/logging/improved_logger'
 require 'madvertise/logging/multi_logger'
-
-module Madvertise
-  class << self
-    # get global logger instance
-    def logger
-      @logger
-    end
-
-    # set global logger instance
-    def logger=(logger)
-      @logger.info("replacing current logger with #{logger.inspect}") if @logger
-      @logger = logger
-    end
-  end
-end
