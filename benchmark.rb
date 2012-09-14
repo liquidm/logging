d5e157925976e45445b884f9f1f8d38ec721220a
Benedikt Böhm <bb@xnull.de>
2012-09-14 07:49:07 +0200

    improve benchmark

--- benchmark.rb
+++ benchmark.rb
@@ -1,20 +1,22 @@
+require 'bundler/setup'
 require 'madvertise-logging'
 require 'benchmark'
 
 $log = Madvertise::Logging::ImprovedLogger.new(:buffer)
 
-STRING_A = "a string".freeze
-STRING_B = "b string".freeze
+STRING_A = ("a string"*10).freeze
+STRING_B = ("b string"*10).freeze
 
 puts ">>> Testing String interpolation vs. concatenation"
 
 n = 2000000
 Benchmark.bmbm do |x|
-  x.report("concat double") { n.times do; STRING_A + STRING_B; end }
-  x.report("concat interp") { n.times do; "#{STRING_A}#{STRING_B}"; end }
+  x.report("append double") { n.times do; "" << STRING_A << STRING_B << STRING_A; end }
+  x.report("concat double") { n.times do; STRING_A + STRING_B + STRING_A; end }
+  x.report("concat interp") { n.times do; "#{STRING_A}#{STRING_B}#{STRING_A}"; end }
 end
 
-n = 100000
+n = 50000
 
 puts
 puts ">>> Testing caller"
@@ -25,14 +27,14 @@ end
 $debug = false
 $log.level = :info
 
-n = 100000
-
 puts
 puts ">>> Testing log.debug with debug disabled (n=#{n})"
 Benchmark.bmbm do |x|
   x.report("debug w/  guard") { n.times do; $log.debug(STRING_A) if $debug; end }
+  x.report("debug w/  block") { n.times do; $log.debug{STRING_A}; end }
+  x.report("debug w/  block + concat") { n.times do; $log.debug{"#{STRING_A}#{STRING_B}#{STRING_A}"}; end }
   x.report("debug w/o guard") { n.times do; $log.debug(STRING_A); end }
-  x.report("debug w/o guard + concat") { n.times do; $log.debug(STRING_A + STRING_B); end }
+  x.report("debug w/o guard + concat") { n.times do; $log.debug("#{STRING_A}#{STRING_B}#{STRING_A}"); end }
 end
 
 $debug = true
@@ -43,7 +45,6 @@ puts ">>> Testing log.debug with debug enabled (n=#{n})"
 Benchmark.bmbm do |x|
   x.report("debug w/  guard") { n.times do; $log.debug(STRING_A) if $debug; end }
   x.report("debug w/o guard") { n.times do; $log.debug(STRING_A); end }
-  x.report("debug w/o guard + concat") { n.times do; $log.debug(STRING_A + STRING_B); end }
 end
 
 #require 'jruby/profiler'
