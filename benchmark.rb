c4d7580863b30507ff52db5154cebcfe46c68b12
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-09-16 14:15:05 +0200

    cleanup benchmark script

--- benchmark.rb
+++ benchmark.rb
@@ -2,14 +2,18 @@ require 'bundler/setup'
 require 'madvertise-logging'
 require 'benchmark'
 
+puts "Using " + %x(ruby -v)
+puts
+
 $log = Madvertise::Logging::ImprovedLogger.new(:buffer)
 
 STRING_A = ("a string"*10).freeze
 STRING_B = ("b string"*10).freeze
 
-puts ">>> Testing String interpolation vs. concatenation"
-
 n = 2000000
+
+puts ">>> Testing String interpolation vs. concatenation (n=#{n})"
+
 Benchmark.bmbm do |x|
   x.report("append double") { n.times do; "" << STRING_A << STRING_B << STRING_A; end }
   x.report("concat double") { n.times do; STRING_A + STRING_B + STRING_A; end }
@@ -46,12 +50,3 @@ Benchmark.bmbm do |x|
   x.report("debug w/  guard") { n.times do; $log.debug(STRING_A) if $debug; end }
   x.report("debug w/o guard") { n.times do; $log.debug(STRING_A); end }
 end
-
-#require 'jruby/profiler'
-#
-#profile_data = JRuby::Profiler.profile do
-#  n.times do; $log.debug(STRING_A); end
-#end
-#
-#profile_printer = JRuby::Profiler::GraphProfilePrinter.new(profile_data)
-#profile_printer.printProfile(STDOUT)
