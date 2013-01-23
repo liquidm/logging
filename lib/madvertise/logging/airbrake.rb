be177e57eefaceedf874296d2a31c709c648452f
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-01-23 13:56:54 +0100

    support xception without airbrake as fallback

--- lib/madvertise/logging/airbrake.rb
+++ lib/madvertise/logging/airbrake.rb
@@ -7,12 +7,15 @@ module Madvertise
       #
       # @param [Exception] exc  The exception to log.
       # @param [String] message  Additional reason to log.
-      def exception(exc, message = nil, attribs = {})
+      def exception_with_airbrake(exc, message = nil, attribs = {})
         Airbrake.notify_or_ignore(exc, {
           :error_message => message,
           :cgi_data => ENV.to_hash,
         }.merge(attribs))
       end
+
+      alias_method :exception_without_airbrake, :exception
+      alias_method :exception, :exception_with_airbrake
     end
   end
 end
